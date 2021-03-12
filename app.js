80d25efc895984ce2987d08d27a2d60564d89391
Bernard Braun <bernard.sbraun@gmail.com>
2021-03-12 01:19:47 -0300

    Efetuada a inserção dos arrays no arquivo TypeScript e em live watch para JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+exports.__esModule = true;
+var jobs = [{
+        id: 1,
+        descricao: 'Realizar o backup do servidor Windows',
+        data_limite: '2021-03-12 14:00:00',
+        horas_job: '6 horas'
+    },
+    {
+        id: 2,
+        descricao: 'Realizar a formatação do servidor Windows',
+        data_limite: '2021-03-15 16:00:00',
+        horas_job: '8 horas'
+    },
+    {
+        id: 3,
+        descricao: 'Realizar a configuração do Active Directory do servidor Windows',
+        data_limite: '2021-03-16 12:00:00',
+        horas_job: '4 horas'
+    },
+    {
+        id: 4,
+        descricao: "Realizar a ativação das GPO's do servidor Windows",
+        data_limite: '2021-12-04 13:00:00',
+        horas_job: '5 horas'
+    },
+    {
+        id: 5,
+        descricao: 'Realizar a configuração do File Server do servidor Windows',
+        data_limite: '2021-12-04 10:00:00',
+        horas_job: '2 horas'
+    }];
