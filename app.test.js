1ea1d028ab8112bda43c76c775ac14c265fc4899
Bernard Braun <bernard.sbraun@gmail.com>
2021-03-12 12:17:05 -0300

    criado arquivo de testes e incluso um teste

---
+++ app.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('isId()', () => {
+it('Deve retornar true se tiver id', () =>{
+    expect(isId(1)).to.equal(true)
+});
+
+});
+
+
